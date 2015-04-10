e2e11e57ff0da9bac841b70b1d00a0816d3d5129
gean felipe <geanfelipemoura@gmail.com>
2015-04-10 12:40:50 -0300

    quase terminado a classe pessoalDAO-falta o redirect

--- src/source/sgis/servlet/PessoalServlet.java
+++ src/source/sgis/servlet/PessoalServlet.java
@@ -1,11 +1,22 @@
 package source.sgis.servlet;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import source.sgis.DAO.PessoalDAO;
+import source.sgis.classes.Pessoal;
 
 /**
  * Servlet implementation class PessoalServlet
@@ -15,8 +26,30 @@ public class PessoalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
+	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		PessoalDAO dao = new PessoalDAO();
+		Pessoal pessoal = new Pessoal();
+		
+		pessoal.setNome(request.getParameter("name"));
+		pessoal.setGenero(request.getParameter("gender"));
+		pessoal.setEstadoCivil(request.getParameter("Cidadao-estadoCivil"));
+		pessoal.setNacionalidade(request.getParameter("Cidadao-nacionalidade"));
+		String dataNascimentoTexto = request.getParameter("Cidadao-nacionalidade");
+		Calendar dataNascimento = null;
+		
+		try{
+			Date data = (Date) new SimpleDateFormat("dd/MM/yyyy").parse(dataNascimentoTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(data);
+			pessoal.setDataNascimento(dataNascimento);
+		}catch(ParseException | java.text.ParseException e){
+			return ;
+			
+		}
+		
+		dao.adicionar(pessoal);
+		
 		
 	}
     /**
